fadb6659a0b62ac318b9a847b51619f271611de2
Deivid <101598816+deividbautista@users.noreply.github.com>
2023-09-18 13:38:35 -0500

    Update

--- src/static/js/SweetAlerts.js
+++ src/static/js/SweetAlerts.js
@@ -3,30 +3,31 @@ const idlabel = document.querySelectorAll(".usuariosSeleccionados");
 const boton = document.getElementById("boton");
 const checkboxes = document.querySelectorAll(".usuariosSelect");
 const inputOculto = document.getElementById('inputOculto');
+// --------------------------------------------------------------------------------
 
 
 // --------------------------------------------------------------------------------
 document.addEventListener("DOMContentLoaded", function () {
-    // Obtiene todos los botones de clase "btnEliminarAsignacion"
+    // Obtiene todos los botones de clase "btnEliminarAsignacion".
     var buttons = document.querySelectorAll(".btnEliminarAsignacion");
   
-    // Agrega un controlador de eventos a cada botón
+    // Agrega un controlador de eventos a cada botón.
     buttons.forEach(function (button) {
       button.addEventListener("click", function (event) {
   
-        event.preventDefault(); // Previene la acción por defecto del botón
-        // Obtiene los atributos "data"
+        event.preventDefault(); // Previene la acción por defecto del botón.
+        // Obtiene los atributos "data".
         var idProceso = button.getAttribute("data-id");
         var idAsignado = button.getAttribute("data-id-asignado");
   
-        // Crea un objeto de datos a enviar en la solicitud
+        // Crea un objeto de datos a enviar en la solicitud.
         var data = {
           id_proceso: idProceso,
           id_asignado: idAsignado
         };
   
-        // Urilizamos la variable swal, la cual nos determinara los valores o caracteristicas 
-        // del alert que estamos exponiendo al usuario
+        // Urilizamos la variable "Swal", la cual nos determinara los valores o caracteristicas 
+        // del alert que estamos exponiendo al usuario.
         Swal.fire({
           title: '¿Estás seguro?',
           text: 'Esta acción eliminará la asignación de forma permanente.',
@@ -34,33 +35,34 @@ document.addEventListener("DOMContentLoaded", function () {
           showCancelButton: true,
           confirmButtonText: 'Sí, eliminar',
           cancelButtonText: 'Cancelar',
+          // Lista con las caracteristicas de estilos personalizados.
           customClass: {
-            // confirmButtonColor: '#900C3F',
-            // cancelButtonColor: '#33415c',
-            // confirmButtonText: 'Sí, eliminar',
-            // cancelButtonText: 'Cancelar'
+            // Apartado de customización de estilos en la alerta.
             confirmButton: 'custom-button-confirmation-1',
             cancelButton: 'custom-button-cancel-1',
             icon: 'custom-icon-class-1'
           }
+
+        // Condicional para determinar si se confirma la eliminación o si se cancela el proceso.
         }).then((result) => {
+          // En caso de ser afirmativo se realizaria el siguiente proceso.
           if (result.isConfirmed) {
-            /// Aquí puedes agregar la lógica para eliminar al usuario
+            // Realizamos la petición al servidor para realizar la eliminación del usuario asignado.
           fetch("/deleteAsignacion", {
             method: "POST",
             headers: {
-              "Content-Type": "application/json" // Establece el tipo de medio como JSON
+              "Content-Type": "application/json" // Establece el tipo de medio como JSON.
             },
             body: JSON.stringify(data)
           })
             .then(response => response.json())
             .then(data => {
-              // Maneja la respuesta del servidor aquí
+              // Maneja la respuesta del servidor aquí.
               console.log(data);
-              // Recarga la página o realiza otras acciones según sea necesario
+              // Recarga la página o realiza otras acciones según sea necesario.
             })
             .catch(error => {
-              // Maneja los errores aquí
+              // Maneja los errores aquí.
               console.error(error);
             });
             Swal.fire(
@@ -68,10 +70,11 @@ document.addEventListener("DOMContentLoaded", function () {
               'La asignación ha sido eliminada.',
               'success'
             ).then(() => {
-              // Refresca la ventana después de la confirmación
+              // Refresca la ventana después de la confirmación.
               location.reload();
             });
-            
+          
+          // En caso de ser negativo el proceso se dara por cancelado y se ilustrara el siguiente SweetAlert.
           } else {
             Swal.fire({
               title: 'Cancelado',
@@ -87,52 +90,49 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
-  // --------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
   
   
-  // -----------------------------------------------------------------------
+// --------------------------------------------------------------------------------
   document.addEventListener('DOMContentLoaded', function () {
     var eliminarButtons = document.querySelectorAll('.eliminar');
   
     eliminarButtons.forEach(function (eliminarButton) {
       eliminarButton.addEventListener('click', function (e) {
-        e.preventDefault(); // Evita que el enlace se abra inmediatamente
-  
+        e.preventDefault(); // Evita que el enlace se abra inmediatamente.
+        
+        // Variable con la información sobre la dirección para hacer peticiones a la api.
         var deleteUrl = this.getAttribute('data-url');
   
-        // Muestra la alerta SweetAlert
+        // Muestra el SweetAlert al usuario.
         Swal.fire({
-          // title: '¿Estás seguro?',
-          // text: 'Esta acción no se puede deshacer',
-          // icon: 'warning',
-          // showCancelButton: true,
-          // confirmButtonColor: '#3085d6',
-          // cancelButtonColor: '#d33',
-          // cancelButtonText: 'Cancelar',
-          // confirmButtonText: 'Sí, borrarlo'
-  
           title: '¿Estás seguro?',
           text: 'Esta acción eliminara el proceso de manera permanente.',
           icon: 'warning',
           showCancelButton: true,
           confirmButtonText: 'Sí, eliminar',
           cancelButtonText: 'Cancelar',
+
           customClass: {
             confirmButton: 'custom-button-confirmation-1',
             cancelButton: 'custom-button-cancel-1',
             icon: 'custom-icon-class-1'
           }
-  
+        
+        // Condicional para determinar si se confirma la eliminación o si se cancela el proceso.
         }).then(function (result) {
+          // Si el usuario confirma, redirige a la dirección de la api para borrar el proceso.
           if (result.isConfirmed) {
-            // Si el usuario confirma, redirige a la página de borrado
             window.location.href = deleteUrl;
+          // En caso de ser negativo el proceso se dara por cancelado y se ilustrara el siguiente SweetAlert.
           } else {
             Swal.fire({
               title: 'Cancelado',
               text: 'La eliminación de la asignación ha sido cancelada.',
               icon: 'info',
+              // Lista con las caracteristicas de estilos personalizados.
               customClass: {
+                // Apartado de customización de estilos en la alerta.
                 confirmButton: 'custom-button-confirmation-2',
                 icon: 'custom-icon-class-2',
               }
@@ -142,4 +142,4 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
-  // --------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
